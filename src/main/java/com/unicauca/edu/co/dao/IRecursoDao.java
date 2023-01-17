d26534b8ad118cffda2778c7fc6a8284e69e079b
unknown <brabadi@unicauca.edu.co>
2023-01-16 22:29:21 -0500

    actualizar

--- src/main/java/com/unicauca/edu/co/dao/IRecursoDao.java
+++ src/main/java/com/unicauca/edu/co/dao/IRecursoDao.java
@@ -15,6 +15,6 @@ public interface IRecursoDao extends CrudRepository<Recurso, Long> {
 	@Query("SELECT r from Recurso r where r.tiporecurso.rectipo_codigo = 'Salon' OR r.tiporecurso.rectipo_codigo = 'Sala' OR r.tiporecurso.rectipo_codigo = 'Auditorio' And r.facultad.fac_codigo = ?1")
 	List<Recurso> recursosPorFacultadAudiSalaSalon (String fac_codigo);
 	
-	@Query("SELECT r from Recurso r where r.tiporecurso.rectipo_codigo <> 'Salon' AND r.tiporecurso.rectipo_codigo <> 'Sala' AND r.tiporecurso.rectipo_codigo <> 'Auditorio' And r.facultad.fac_codigo = ?1 AND r.estado = false")
+	@Query("SELECT r from Recurso r where r.tiporecurso.rectipo_codigo <> 'Salon' AND r.tiporecurso.rectipo_codigo <> 'Sala' AND r.tiporecurso.rectipo_codigo <> 'Auditorio' And r.facultad.fac_codigo = ?1 AND r.estado = true")
 	List<Recurso> recursosPorFacultadDiferenteAudiSalaSalon(String fac_codigo);
 }
