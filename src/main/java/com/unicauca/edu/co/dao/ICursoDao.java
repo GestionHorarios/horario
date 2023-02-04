b6c62b307269f75d4f24c85e3ed1b1e9455fe1cc
unknown <brabadi@unicauca.edu.co>
2023-02-04 12:18:21 -0500

    Curso

--- src/main/java/com/unicauca/edu/co/dao/ICursoDao.java
+++ src/main/java/com/unicauca/edu/co/dao/ICursoDao.java
@@ -1,9 +1,13 @@
 package com.unicauca.edu.co.dao;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
 
 import com.unicauca.edu.co.model.Curso;
 
 public interface ICursoDao extends CrudRepository<Curso, Long> {
-
+    @Query("SELECT c from Curso c where c.cur_id LIKE %?1%")
+    List<Curso> findByCur_id(String cur_id);
 }
